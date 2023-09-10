1de4cf57e7e13aa896092520ec04ddcb0861c8ca
Evo Stamatov <aviolito@gmail.com>
2023-09-11 01:45:03 +1000

    chore: version bump to 0.5-beta1

--- up.go
+++ up.go
@@ -38,7 +38,7 @@ import (
 	"github.com/spf13/pflag"
 )
 
-const version = "0.4 (2020-10-29)"
+const version = "0.5-beta1 (2023-09-10)"
 
 // TODO: in case of error, show it in red (bg?), then below show again initial normal output (see also #4)
 // TODO: F1 should display help, and it should be multi-line, and scrolling licensing credits
